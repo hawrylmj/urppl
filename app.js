5f7e28c036991fa0ed08cdaa4422c336ed4bd12b
Gordon Kristan <gordon.kristan@gmail.com>
2013-11-16 11:56:29 -0500

    One more module removal.

--- app.js
+++ app.js
@@ -15,15 +15,11 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
